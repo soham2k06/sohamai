8f05f25eebb3ec8511b646a01b9e23de0c556af9
soham2k06 <sohmm.dev@gmail.com>
2024-07-12 11:59:56 +0530

    feat: preventing user navigating to auth routes after when already signed in

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,8 +1,14 @@
-export default function Layout({
+import { getUser } from "@/lib/actions/auth";
+import { redirect } from "next/navigation";
+
+export default async function Layout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const user = await getUser();
+
+  if (user.aud === "authenticated") redirect("/");
   return (
     <main className="p-4 container min-h-screen flex items-center">
       {children}
